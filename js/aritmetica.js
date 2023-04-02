b3a4a90915ef969893045ef56670ba0a3888c285
walter <wrangelfuentes@gmail.com>
2023-04-01 20:41:28 -0500

    modulos

---
+++ js/aritmetica.js
@@ -0,0 +1,4 @@
+
+export function suma(a, b) {
+    return a + b;
+} 
---
+++ js/modulos.js
@@ -0,0 +1,11 @@
+import{PI} from "./constantes.js"
+import{suma} from "./aritmetica.js"
+
+console.log("Archivo modulos.js")
+console.log(PI)
+console.log(suma(3, 4));
+
+
+
+
+
